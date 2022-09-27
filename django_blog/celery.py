8427fc56d48bb0fbfea8fa3b9e0c44129b7e2c5b
ig.rudenko <ig.rudenko@users.noreply.github.com>
2022-09-27 21:59:21 +0300

    refactoring

--- django_blog/celery.py
+++ django_blog/celery.py
@@ -5,3 +5,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog.settings')
 app = Celery('django_blog')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+#
